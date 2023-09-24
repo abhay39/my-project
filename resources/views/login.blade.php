464dea553506dc7d646add28b387ec81823af24f
abhay39 <abhayguptaak39@gmail.com>
2023-09-24 13:11:47 -0700

    initial

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -7,15 +7,17 @@
 </head>
 
 <body>
-  <div class="h-[100vh] flex items-center justify-center">
-    <div class=" justify-center items-center  bg-slate-500 p-6 space-x-5 space-y-5 rounded">
-      <form>
-        <h1 class="text-4xl font-bold text-white text-center">
+  <div class="h-[100vh] flex ">
+    <div class="bg-black w-1/3"></div>
+
+    <div class="flex justify-center w-2/3 items-center  p-6 space-x-5 space-y-5  ">
+      <form class="bg-slate-300 p-5 rounded">
+        <h1 class="text-4xl font-bold text-black text-center">
           Login
         </h1>
 
        <div class="mt-2">
-        <label class="text-base text-white">
+        <label class="text-base text-black">
           Username
         </label>
         <br />
@@ -23,19 +25,19 @@
        </div>
 
        <div class="mt-2">
-        <label class="text-base text-white">
+        <label class="text-base text-black">
           Password
         </label>
         <br />
         <input type="password" placeholder="Enter your username" class="p-2 rounded border-green-300 outline-none sm:w-[100%] md:w-[250px]"/>
        </div>
-       <input type="submit" value="Login" class="p-2 bg-cyan-700 items-center justify-center text-center mt-2 sm:w-[100%] md:w-[250px] rounded text-white text-lg cursor-pointer" />
+       <div>
+        <input type="submit" value="Login" class="p-2 bg-cyan-700 items-center justify-center text-center mt-2 sm:w-[100%] md:w-[250px] rounded text-white text-lg cursor-pointer" />
+       </div>
        <div class="flex">
          <p class="text-sm text-center">Don't have an account?  
-        <p class="text-sm text-center cursor-pointer underline text-white"> SignUp</p>
-       </div>
-        
-      </p>
+        <p class="text-sm text-center cursor-pointer underline text-black user-select:none"> SignUp</p>
+       </div> 
       </form>
     </div>
   </div>
